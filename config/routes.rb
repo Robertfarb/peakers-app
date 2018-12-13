5fdb4ed674e9a6062deb3bf42c4c31ea1ee13324
Robert Farb <farb.robert@gmail.com>
2018-12-13 13:01:14 -0800

    Add routes to rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :geocode, only: [:index]
+  resources :reverse_geocode, only: [:index]
+  resources :get_distance, only: [:index]
+
   root "static_pages#root"
 end
