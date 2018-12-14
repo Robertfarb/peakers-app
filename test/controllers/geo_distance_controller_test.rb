29925abca505682842297c0705583e2c69644327
Robert Farb <farb.robert@gmail.com>
2018-12-14 14:39:41 -0800

    Complete tests for application

--- test/controllers/geo_distance_controller_test.rb
+++ test/controllers/geo_distance_controller_test.rb
@@ -3,4 +3,11 @@ class GeodistanceControllerTest < ActionDispatch::IntegrationTest
     get "/geo_distance"
     assert_response :success
   end
+
+  test "should correctly handle invalid lat / lon inputs" do
+    get "/geo_distance?lat1=asdf&lon1=asdf&lat2=asdf&lon2=asdf"
+    response = JSON.parse(@response.body)
+    assert_equal("Please enter valid latitude and longitude", response["geo_distance"]["distance"])
+    assert_response :success
+  end
 end
